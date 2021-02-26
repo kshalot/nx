8501d8ff6c62b234993cbc9ef62375dd667ac4ad
Sean Moriarity <smoriarity.5@gmail.com>
2021-02-26 15:48:29 -0500

    Add example

--- exla/examples/tpu.exs
+++ exla/examples/tpu.exs
@@ -1,1 +1,7 @@
-EXLA.NIF.get_tpu_client()
+defmodule Softmax do
+  @default_defn_compiler {Exla, client: :tpu, max_float_type: {:f, 32}}
+
+  defn softmax(t), do: Nx.exp(t) / Nx.sum(Nx.exp(t))
+end
+
+IO.inspect Softmax.softmax(Nx.tensor([1, 2, 3, 4], type: {:f, 32}))
